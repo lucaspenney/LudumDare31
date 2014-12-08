24b616203cd80a35a286e529576f41b30f1f03f9
Lucas Penney <lucaspenney@gmail.com>
2014-12-08 14:11:55 -0800

    Add rotational velocity to player, thrust values

--- js/player.js
+++ js/player.js
@@ -6,9 +6,12 @@ function Player(game, x, y) {
 	this.sprite = new Sprite(this.game, this, "img/player.png");
 	this.xv = 0;
 	this.yv = 0;
+	this.rv = 0;
 	this.layer = 100;
 	this.trail = new Trail(this.game, 0, 0, this, 8);
 	this.enginesOn = false;
+	this.turnThrust = 0.35;
+	this.mainThrust = 0.4;
 }
 
 Player.prototype.update = function() {
@@ -16,8 +19,8 @@ Player.prototype.update = function() {
 	this.enginesOn = false;
 	if (this.game.input.keys[38] || this.game.input.keys[87]) {
 		this.enginesOn = true;
-		this.xv += Math.cos(degToRad(this.rotation - 90)) * 0.4;
-		this.yv += Math.sin(degToRad(this.rotation - 90)) * 0.4;
+		this.xv += Math.cos(degToRad(this.rotation - 90)) * this.mainThrust;
+		this.yv += Math.sin(degToRad(this.rotation - 90)) * this.mainThrust;;
 		if (this.xv > 10) this.xv = 10;
 		if (this.yv > 10) this.yv = 10;
 		if (this.xv < -10) this.xv = -10;
@@ -25,17 +28,21 @@ Player.prototype.update = function() {
 	}
 	if (this.game.input.keys[37] || this.game.input.keys[65]) { //Left Arrow
 		this.enginesOn = true;
-		this.rotation -= 5;
+		this.rv -= this.turnThrust;;
 	}
 	if (this.game.input.keys[39] || this.game.input.keys[68]) { //right arrow
 		this.enginesOn = true;
-		this.rotation += 5;
+		this.rv += this.turnThrust;
 	}
 	this.x += this.xv;
 	this.y += this.yv;
+	this.rotation += this.rv;
+	if (this.rv > 10) this.rv = 10;
+	if (this.rv < -10) this.rv = -10;
 	if (Math.random() > 0.5) {
 		this.xv = this.xv * 0.99;
 		this.yv = this.yv * 0.99;
+		this.rv = this.rv * 0.99;
 	}
 
 	this.game.screen.setXOffset(this.x - 350);
